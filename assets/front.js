760cd965bbe95e04b01541bceceb45f08d6d7303
Shyhyro <Bosquet.laurie@outlook.com>
2021-11-29 11:16:09 +0100

    update

--- assets/front.js
+++ assets/front.js
@@ -4,15 +4,22 @@ import {Link} from "./js/link.mjs";
 import {Effect} from "./js/effect.mjs";
 import {Position} from "./js/position";
 
-
 new Dock().create();
-
 new Position().panel();
 new Position().position();
 
 new Link().addLink("Github");
 new Link().addLink("Google");
 new Link().addLink("Github");
+new Link().addLink("Google");
+new Link().addLink("Github");
+new Link().addLink("Google");
+new Link().addLink("Github");
+new Link().addLink("Google");
+new Link().addLink("Github");
+new Link().addLink("Google");
+new Link().addLink("Github");
+new Link().addLink("Google");
 new Link().addLink("Google");
 new Link().addLink("Github");
 new Link().addLink("Google");
